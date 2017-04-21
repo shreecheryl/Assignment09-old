3909775acc75dba2cd4b54a3083033f5c4dc2ff0
shreecheryl <shreecheryl@att.net>
2017-04-21 10:10:59 -0700

    in progress 042117

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+$(function () {
+    "use strict";
+    
+    // General Tool Tip
+    $(document).tooltip();
+    
+    // Home Page Tabs
+    $("#tabs").tabs();
+    
+    // Home Page Accordion
+    $("#accordion").accordion();
+    
+    // About Us Tabs
+    
+    $("#tabsManagement").tabs().addClass("ui-tabs-vertical ui-helper-clearfix");
+    $("#tabsManagement li").removeClass("ui-corner-top").addClass("ui-corner-left");
+
+}); // end of document ready
+        
